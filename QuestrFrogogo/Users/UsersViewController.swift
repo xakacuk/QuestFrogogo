964e42a843b71c002bb987415f5e113d6a30fd6a
xakacuk <assky@mail.ru>
2019-10-18 14:33:12 +0700

    add refresh and spinner getUsers

--- QuestrFrogogo/Users/UsersViewController.swift
+++ QuestrFrogogo/Users/UsersViewController.swift
@@ -10,12 +10,14 @@ import UIKit
 
 private enum Cons: String {
     case userCell = "userCell"
+    case selectedUserSegue = "selectedUserSegue"
 }
 
 final class UsersViewController: UIViewController {
     
     private let model = ModelUsersViewController()
     private var users = [User]()
+    private var refreshControl: UIRefreshControl!
     
     @IBOutlet weak var usersTableView: UITableView! {
         didSet {
@@ -25,6 +27,7 @@ final class UsersViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupRefresh()
         getUsers()
     }
     
@@ -34,17 +37,37 @@ final class UsersViewController: UIViewController {
         usersTableView.reloadData()
     }
     
+    private func setupRefresh() {
+        refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: #selector(refresh), for: .valueChanged)
+        usersTableView.refreshControl = refreshControl
+    }
+    
+    @objc private func refresh() {
+        getUsers()
+    }
+    
     private func getUsers() {
+        refreshControl.beginRefreshing()
         model.getUsers() { result in
             switch result {
                 case .success(let response):
                     self.users = response
                     self.setupTableView()
+                    self.refreshControl.endRefreshing()
                     break
                 case .failure(let error):
                     print(error.localizedDescription)
+                    self.refreshControl.endRefreshing()
                     break
             }
+        }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == Cons.selectedUserSegue.rawValue {
+//            let controller = segue.destination as! FullImageViewController
+//            controller.wallpaper = selectedImage
         }
     }
 }
@@ -56,6 +79,7 @@ extension UsersViewController: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: Cons.userCell.rawValue, for: indexPath) as! UserTableViewCell
+        cell.avatarImageView.image = nil
         cell.configureCell(user: users[indexPath.row])
         return cell
     }
@@ -66,5 +90,8 @@ extension UsersViewController: UITableViewDataSource {
 }
 
 extension UsersViewController: UITableViewDelegate {
-    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.cellForRow(at: indexPath)?.isSelected = false
+        performSegue(withIdentifier: Cons.selectedUserSegue.rawValue, sender: self)
+    }
 }
