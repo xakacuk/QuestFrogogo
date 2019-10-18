ceea94a5fccacd12a2542ee71d0d7cb395b54b6d
xakacuk <assky@mail.ru>
2019-10-18 12:08:23 +0700

    init commit

---
+++ QuestrFrogogo/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  QuestrFrogogo
+//
+//  Created by Евгений Бейнар on 18.10.2019.
+//  Copyright © 2019 Евгений Бейнар. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        return true
+    }
+}
+
---
+++ QuestrFrogogo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QuestrFrogogo
+//
+//  Created by Евгений Бейнар on 18.10.2019.
+//  Copyright © 2019 Евгений Бейнар. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
